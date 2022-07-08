6b7a5fa6d7cbac8c6514ad8a7c90b138e66c91be
JossTorres91 <ingjmtorres086@gmail.com>
2022-07-07 20:04:55 -0500

    Creación de aMatriz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hola Mundo");
+console.log("Ejemplo de una Matriz");
+
+const aNumber = [2, 0, 1];
+const bNumber = [3, 0 , 0];
+const cNumber = [5, 1, 1];
+
+const aMatriz = [aNumber, bNumber, cNumber];
+
+console.log(aMatriz);
